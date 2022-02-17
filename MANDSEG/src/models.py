831d271008af9de4e2dbbd4046c3c64630a7b83c
Maxlo24 <max.gillot.69@gmail.com>
2022-02-17 13:51:43 -0500

    new model

--- MANDSEG/src/models.py
+++ MANDSEG/src/models.py
@@ -1,22 +1,32 @@
-from monai.networks.nets import UNETR
+from monai.networks.nets import UNETR,UNet
 
 def Create_UNETR(input_channel, label_nbr,cropSize):
 
-    model = UNETR(
+    # model = UNETR(
+    #     in_channels=input_channel,
+    #     out_channels=label_nbr,
+    #     img_size=cropSize,
+    #     feature_size=16,
+    #     hidden_size=768,
+    #     mlp_dim=3072,
+    #     num_heads=12,
+    #     # feature_size=32,
+    #     # hidden_size=1024,
+    #     # mlp_dim=4096,
+    #     # num_heads=16,
+    #     pos_embed="perceptron",
+    #     norm_name="instance",
+    #     res_block=True,
+    #     dropout_rate=0.05,
+    # )
+
+
+    model = UNet(
+        spatial_dims=3,
         in_channels=input_channel,
         out_channels=label_nbr,
-        img_size=cropSize,
-        feature_size=16,
-        hidden_size=768,
-        mlp_dim=3072,
-        num_heads=12,
-        # feature_size=32,
-        # hidden_size=1024,
-        # mlp_dim=4096,
-        # num_heads=16,
-        pos_embed="perceptron",
-        norm_name="instance",
-        res_block=True,
+        channels = (16,32,64,128,256),
+        strides=(2,2,2,2),
         dropout_rate=0.05,
     )
 
